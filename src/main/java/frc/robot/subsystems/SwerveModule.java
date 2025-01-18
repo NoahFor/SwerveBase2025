dc086e88a578c6b058c6ba4c4d35e66e0a94f7fe
Noah Forseth <forseth.noah@gmail.com>
2025-01-18 14:11:32 -0700

    initial commit

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -7,28 +7,32 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.kauilabs.navx.frc.AHRS;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.AnalogInput;
-import com.revrobotics.SparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ModuleConstants;
 
-import com.ctre.phoenix6.hardware.CANcoder;
-
 public class SwerveModule extends SubsystemBase {
 
   private SparkMax driveMotor;
-  private SparkMarx turningMotor;
+  private SparkMax turningMotor;
   // built in encoders
   private RelativeEncoder driveEncoder;
   private RelativeEncoder turningEncoder;
@@ -44,40 +48,125 @@ public class SwerveModule extends SubsystemBase {
   private double absoluteEncoderOffsetRad;
   private int turningMotorId;
   private int absoluteEncoderId;
-  
-  public ExampleSubsystem() {}
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+private SparkMaxConfig config;
+
+  public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+    int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+      config = new SparkMaxConfig();
+      this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+      this.absoluteEncoderReversed = absoluteEncoderReversed;
+
+      this.absoluteEncoder = new CANcoder(absoluteEncoderId);
+
+      driveMotor = new SparkMax(driveMotorId, MotorType.kBrushless);
+      turningMotor = new SparkMax(turningMotorId, MotorType.kBrushless);
+      this.turningMotorId = turningMotorId;
+      this.absoluteEncoderId=absoluteEncoderId;
+
+      driveEncoder = driveMotor.getEncoder();
+      turningEncoder = turningMotor.getEncoder();
+
+      config
+        .inverted(driveMotorReversed)
+        .idleMode(IdleMode.kCoast);
+      config.encoder
+        .positionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter)
+        .velocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+      
+      driveMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);  
+
+      config
+        .inverted(turningMotorReversed)
+        .idleMode(IdleMode.kCoast);
+      config.encoder
+        .positionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad)
+        .velocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+      config.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .pid(ModuleConstants.kPTurning, 0, 0);
+      turningMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+      
+      }
+  //gets drive encoder position in meters
+  public double getDrivePosition(){
+    return driveEncoder.getPosition();
+  }
+  //gets turning encoder position in radians
+  public double getTurningPosition(){
+    return turningEncoder.getPosition();
+  }
+  //gets swerve module position
+  public SwerveModulePosition getPosition(){
+    return new SwerveModulePosition(getDrivePosition(),new Rotation2d(getTurningPosition()));
+  }
+  //gets drive encoder velocity in m/s
+  public double getDriveVelocity(){
+    return driveEncoder.getVelocity();
+  }
+  //gets turning encoder velocity in rad/s
+  public double getTurningVelocity(){
+    return turningEncoder.getVelocity();
+  }
+//
+public double getAbsoluteEncoderRad(){
+  //getAbsolutePosition returns value from -0.5 to 0.5 (circle, -0.5 is next to 0.5)
+  double angle = absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+  //boom angle is now in radians
+  angle *=(2*Math.PI);
+  //offsets the angle according to typed constants
+  //tip: use Pheonix tuner x to zero the absolute encoders manually instead
+  //Every time absolute encoders are unplugged they need to be re-zeroed
+  angle -= absoluteEncoderOffsetRad;
+  //if they are reversed turn it negative
+
+  if(absoluteEncoderReversed){
+    return angle*(-1.0);
+  }
+  else
+    return angle;
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
+public double getAbsoluteEncoderReading(){
+  return getAbsoluteEncoderRad();
+}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+public void resetEncoders(){
+  //zeros drive encoder
+  driveEncoder.setPosition(0);
+  turningEncoder.setPosition(getAbsoluteEncoderRad());
+}
+//gets absolute position of absolute encoders on range of -0.5 to 0.5 (raw data, no conversions)
+public double getAbsolutePos(){
+  return absoluteEncoder.getAbsolutePosition().getValueAsDouble();
+}
+//gets the state of the module
+public SwerveModuleState getState(){
+  //creates a current state from the current drive velocity and turning position
+  return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+}
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+public void setDesiredState(SwerveModuleState state){
+  //does not set a state if there is no speed in the new state
+  if(Math.abs(state.speedMetersPerSecond)<0.001){
+    stop();
+    return;
   }
+  //optimize makes it so that the wheel only has to turn a max of 90 degrees at a time
+  //uses both directions of drive motor instead of just forward
+  state = SwerveModuleState.optimize(state, getState().angle);
+  //sets the driveMotor speed and scale down using physical max meters per second of the motor
+  driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+  //sets the turningMotor position using a pid controller with inputs of the current position of turning motor
+  //combined with the desired position
+  turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+  //
+  //SmartDashboard.putString("Swerve[" + turningMotorId+"] state", state.toString());
+
+}
+//stop
+public void stop(){
+  driveMotor.set(0);
+  turningMotor.set(0);
+}
 }
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,47 +1,152 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import com.studica.frc.AHRS;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
+public class SwerveSubsystem extends SubsystemBase{
+      private Spark LEDStrip = new Spark(0);
+   //creates module instances using the swerveModule.java file
+    private final SwerveModule frontLeft = new SwerveModule(
+        DriveConstants.kFrontLeftDriveMotorPort, 
+        DriveConstants.kFrontLeftTurningMotorPort, 
+        DriveConstants.kFrontLeftDriveEncoderReversed, 
+        DriveConstants.kFrontLeftTurningEncoderReversed, 
+        DriveConstants.kFrontLeftDriveAbsoluteEncoderPort, 
+        DriveConstants.kFrontLeftDriveAbsoluteEncoderOffsetRad, 
+        DriveConstants.kFrontLeftDriveAbsoluteEncoderReversed
+        );
+    private final SwerveModule frontRight = new SwerveModule(
+        DriveConstants.kFrontRightDriveMotorPort, 
+        DriveConstants.kFrontRightTurningMotorPort, 
+        DriveConstants.kFrontRightDriveEncoderReversed, 
+        DriveConstants.kFrontRightTurningEncoderReversed, 
+        DriveConstants.kFrontRightDriveAbsoluteEncoderPort, 
+        DriveConstants.kFrontRightDriveAbsoluteEncoderOffsetRad, 
+        DriveConstants.kFrontRightDriveAbsoluteEncoderReversed
+        );
+    private final SwerveModule backLeft = new SwerveModule(
+        DriveConstants.kBackLeftDriveMotorPort, 
+        DriveConstants.kBackLeftTurningMotorPort, 
+        DriveConstants.kBackLeftDriveEncoderReversed, 
+        DriveConstants.kBackLeftTurningEncoderReversed, 
+        DriveConstants.kBackLeftDriveAbsoluteEncoderPort, 
+        DriveConstants.kBackLeftDriveAbsoluteEncoderOffsetRad, 
+        DriveConstants.kBackLeftDriveAbsoluteEncoderReversed
+        );
+    private final SwerveModule backRight = new SwerveModule(
+        DriveConstants.kBackRightDriveMotorPort, 
+        DriveConstants.kBackRightTurningMotorPort, 
+        DriveConstants.kBackRightDriveEncoderReversed, 
+        DriveConstants.kBackRightTurningEncoderReversed, 
+        DriveConstants.kBackRightDriveAbsoluteEncoderPort, 
+        DriveConstants.kBackRightDriveAbsoluteEncoderOffsetRad, 
+        DriveConstants.kBackRightDriveAbsoluteEncoderReversed
+        );
+    //creates a navX gyro to use in da calcs
+    private AHRS gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
+    //private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(DriveConstants.kDriveKinematics,new Rotation2d(0),
+        new SwerveModulePosition[]{frontLeft.getPosition(),frontRight.getPosition(),backLeft.getPosition(),backRight.getPosition()});
+    
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
+    //cant run zero heading immediatly bc gyro will be booting up and stuf
+    public SwerveSubsystem() {
+        // gyro.calibrate();
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                zeroHeading();
+            } catch (Exception e) {
+            }
+        }).start();
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public void zeroHeading() {
+        gyro.reset();
+    }
+    //gets the heading returned as the gyro reading remainder after being divided by 360
+    //that way it always reads from 0 to 360
+    //or 0 to -360
+    public double getHeading() {
+        return Math.IEEEremainder(gyro.getAngle(), 360);
+    }
+    //returns as radians?
+    public Rotation2d getRotation2d() {
+        return Rotation2d.fromDegrees(getHeading());
+    }
+    //returns Pose with x,y, and theta coordinates of robot
+    public Pose2d getPose(){
+        return odometer.getPoseMeters();
+    }
+    //reset the odometer current theta, module positions
+    public void resetOdometry(Pose2d pose){
+        odometer.resetPosition(getRotation2d(), 
+        new SwerveModulePosition[]{frontLeft.getPosition(),frontRight.getPosition(),backLeft.getPosition(),backRight.getPosition()},
+         pose);
+    }
+    @Override
+    public void periodic() {
+        //rainbow 
+        LEDStrip.set(-0.95);
+        //updates odometer based on new positions which take into account turn encoder and drive encoder along with position on robot
+        odometer.update(getRotation2d(),new SwerveModulePosition[]{frontLeft.getPosition(),frontRight.getPosition(),backLeft.getPosition(),backRight.getPosition()} );
+        //SmartDashboard.putString("frontLeft", frontLeft.getPosition().toString());
+        //SmartDashboard.putString("frontRight", frontRight.getPosition().toString());
+        //SmartDashboard.putString("backLeft", backLeft.getPosition().toString());
+        //SmartDashboard.putString("backRight", backRight.getPosition().toString());
+        //SmartDashboard.putString("Robot Heading", getRotation2d().toString());
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+        // SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
+        // SmartDashboard.putNumber("Absolute Encoder 20",backLeft.getAbsoluteEncoderReading() );
+        // SmartDashboard.putNumber("Absolute Encoder 21",frontLeft.getAbsoluteEncoderReading() );
+        // SmartDashboard.putNumber("Absolute Encoder 22",backRight.getAbsoluteEncoderReading() );
+        // SmartDashboard.putNumber("Absolute Encoder 23",frontRight.getAbsoluteEncoderReading() );
+        
+        // SmartDashboard.putNumber("Relative Encoder 5",backLeft.getTurningPosition() );
+        // SmartDashboard.putNumber("Relative Encoder 13",frontLeft.getTurningPosition() );
+        // SmartDashboard.putNumber("Relative Encoder 8",backRight.getTurningPosition() );
+        // SmartDashboard.putNumber("Relative Encoder 14",frontRight.getTurningPosition() );
+
+        // SmartDashboard.putNumber("Drive Encoder 3",backLeft.getDriveVelocity() );
+        // SmartDashboard.putNumber("Drive Encoder 11",frontLeft.getDriveVelocity() );
+        // SmartDashboard.putNumber("Drive Encoder 10",backRight.getDriveVelocity() );
+        // SmartDashboard.putNumber("Drive Encoder 4",frontRight.getDriveVelocity() );
+
+        
+
+        
+        
+        
+
+    }   
+    //stops modules
+    public void stopModules() {
+        frontLeft.stop();
+        frontRight.stop();
+        backLeft.stop();
+        backRight.stop();
+    }
+    //sets the module states using an array of input desired states
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        frontLeft.setDesiredState(desiredStates[0]);
+        frontRight.setDesiredState(desiredStates[1]);
+        backLeft.setDesiredState(desiredStates[2]);
+        backRight.setDesiredState(desiredStates[3]);
+    }
 }
